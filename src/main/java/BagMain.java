c072faae78e474496db3b1ef230b106cebdcb8b1
Theodore <theo@pta.gg>
2022-09-28 10:52:29 -0400

    Fix reading error.

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -17,7 +17,7 @@ class BagMain {
         for (Bag bag : bags) {
             bag.enhance();
 
-            if (double_enhance_handbags) {
+            if (double_enhance_handbags && bag instanceof HandBag) {
                 bag.enhance();
             }
         }
